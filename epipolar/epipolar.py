1fb4f26af3c7c40cb09f464a7270de327282fa0e
Oh-Hun Kwon <kirloyal@gmail.com>
2020-06-27 23:53:03 +0200

    line color

--- epipolar/epipolar.py
+++ epipolar/epipolar.py
@@ -6,6 +6,7 @@ import os
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 camera_matrix0 =  np.loadtxt( "sample_imgs/epi0_calib.txt" )
 camera_matrix1 =  np.loadtxt( "sample_imgs/epi1_calib.txt" )
 
@@ -33,21 +34,34 @@ def get_cross(A):
     return np.array([[0, -A[2], A[1]], [A[2], 0, -A[0]], [-A[1], A[0], 0]])
 F = get_cross(camera_matrix1 @ C0) @ camera_matrix1 @ np.linalg.pinv(camera_matrix0)
 
-# x1 @ F 
+pt_names0 = pd0.loc[:,0].tolist()
+pt_names1 = pd1.loc[:,0].tolist()
+
+pt_names_all = list(set(pt_names0).union(set(pt_names1)))
+colors = {}
+for pt_name in pt_names_all:
+    colors[pt_name] = np.random.randint(256, size=3)
+
 pts0 = pd0.loc[:,1:].to_numpy()
 for i in range(len(pts0)):
+    pt_name0 = pt_names0[i]
+    color = colors[pt_name0]
     pt0 = pts0[i]
-    cv2.circle(img0, tuple(pts0[i].astype(int).tolist()), 5, (0,0,0),2)
-    l1 = np.append(pt0,1) @ F.T
-    cv2.line(img1, (0, int(-l1[2]/l1[1])), (w1 , int(-(l1[0]*w1+l1[2])/l1[1])), (255,0,0), 1)
+    cv2.circle(img0, tuple(pts0[i].astype(int).tolist()), 5, color.tolist(), 2)
+    if pt_name0 in pt_names1:
+        l1 = np.append(pt0,1) @ F.T
+        cv2.line(img1, (0, int(-l1[2]/l1[1])), (w1 , int(-(l1[0]*w1+l1[2])/l1[1])), color.tolist(), 1)
 
 
 pts1 = pd1.loc[:,1:].to_numpy()
 for i in range(len(pts1)):
+    pt_name1 = pt_names1[i]
+    color = colors[pt_name1]
     pt1 = pts1[i]
-    cv2.circle(img1, tuple(pts1[i].astype(int).tolist()), 5, (0,0,0),2)
-    l0 = np.append(pt1,1) @ F
-    cv2.line(img0, (0, int(-l0[2]/l0[1])), (w0 , int(-(l0[0]*w0+l0[2])/l0[1])), (255,0,0), 1)
+    cv2.circle(img1, tuple(pts1[i].astype(int).tolist()), 5, color.tolist(),2)
+    if pt_name1 in pt_names0:
+        l0 = np.append(pt1,1) @ F
+        cv2.line(img0, (0, int(-l0[2]/l0[1])), (w0 , int(-(l0[0]*w0+l0[2])/l0[1])), color.tolist(), 1)
 
 cv2.imwrite('results/epi0.jpg', img0)
 cv2.imwrite('results/epi1.jpg', img1)
